c7a17aa24ae2230a2f37ed1e0d1ccf6e90ae5931
Kanto <66814471+kanarch66@users.noreply.github.com>
2025-01-05 11:51:21 +0700

    Update flux-model-download.sh

--- flux-model-download.sh
+++ flux-model-download.sh
@@ -1,16 +1,18 @@
 # Create the necessary directories for the files
-mkdir -p /workspace/ComfyUI/models/unet
-mkdir -p /workspace/ComfyUI/models/vae
-mkdir -p /workspace/ComfyUI/models/clip
+[ ! -d "/workspace/ComfyUI/models/unet" ] && mkdir -p /workspace/ComfyUI/models/unet
+[ ! -d "/workspace/ComfyUI/models/vae" ] && mkdir -p /workspace/ComfyUI/models/vae
+[ ! -d "/workspace/ComfyUI/models/clip" ] && mkdir -p /workspace/ComfyUI/models/clip
+[ ! -d "/workspace/ComfyUI/models/controlnet" ] && mkdir -p /workspace/ComfyUI/models/controlnet
 
 # Print completion message
-echo "Downloading approx 17.5 GB. Please wait."
+echo "Downloading approx 20 GB. Please wait."
 
 # Download the safetensors files into the specified directories
 wget -P /workspace/ComfyUI/models/unet https://huggingface.co/Kijai/flux-fp8/resolve/main/flux1-dev-fp8-e4m3fn.safetensors
 wget -P /workspace/ComfyUI/models/vae https://huggingface.co/Kijai/flux-fp8/resolve/main/flux-vae-bf16.safetensors
 wget -P /workspace/ComfyUI/models/clip https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors
 wget -P /workspace/ComfyUI/models/clip https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors
+wget -p /workspace/ComfyUI/models/controlnet https://huggingface.co/Kijai/flux-fp8/resolve/main/flux_shakker_labs_union_pro-fp8_e4m3fn.safetensors
 
 # Print completion message in rainbow colors
 echo -e "\e[31mF\e[33ml\e[32mu\e[36mx\e[34m \e[35md\e[31mo\e[33mw\e[32mn\e[36ml\e[34mo\e[35ma\e[31md\e[33ms \e[32mc\e[36mo\e[34mm\e[35mp\e[31ml\e[33me\e[32mt\e[36me\e[34md \e[35ms\e[31mu\e[33mc\e[32mc\e[36me\e[34ms\e[35ms\e[31mf\e[33fu\e[32ml\e[36ly."
